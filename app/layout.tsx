bcc267c84ac0e0b2292f57a12717f4b965fd084b
ananda-G <anandagilang007@gmail.com>
2025-12-14 22:54:13 +0700

    perubahan

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
