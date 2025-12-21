29e6fa2222453f08e1cfc11860474bb2b0d9ca5e
ananda-G <anandagilang007@gmail.com>
2025-12-21 21:38:31 +0700

    pembetulan error

--- auth.ts
+++ auth.ts
@@ -2,25 +2,22 @@ import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
-import type { User } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
 import postgres from 'postgres';
+import bcrypt from 'bcrypt';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-async function getUser(email: string): Promise<User | undefined> {
-  try {
-    const user = await sql<User[]>`
-      SELECT * FROM users WHERE email = ${email}
-    `;
-    return user[0];
-  } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
-  }
+async function getUser(email: string) {
+  const users = await sql`
+    SELECT id, name, email, password
+    FROM users
+    WHERE email = ${email}
+  `;
+  return users[0];
 }
 
 export const { auth, signIn, signOut } = NextAuth({
+  secret: process.env.AUTH_SECRET,
   ...authConfig,
   providers: [
     Credentials({
@@ -32,19 +29,25 @@ export const { auth, signIn, signOut } = NextAuth({
           })
           .safeParse(credentials);
 
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
+        if (!parsedCredentials.success) return null;
 
-          if (!user) return null;
+        const { email, password } = parsedCredentials.data;
+        const user = await getUser(email);
 
-          const passwordsMatch = await bcrypt.compare(
-            password,
-            user.password
-          );
-        }
+        if (!user) return null;
 
-        return null;
+        const passwordsMatch = await bcrypt.compare(
+          password,
+          user.password,
+        );
+
+        if (!passwordsMatch) return null;
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        };
       },
     }),
   ],
